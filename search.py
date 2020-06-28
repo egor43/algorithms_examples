2233b291e03c059dd0931094cb9e3ce9347bf08b
egor43 <egor43.rus@mail.ru>
2020-06-29 00:35:56 +0300

    Реализована функция поиска кротчайшего пути по алгоритму Дейкстры

--- search.py
+++ search.py
@@ -58,6 +58,89 @@ def graph_search(algorithm, root_node, check_func):
     return None
 
 
+def _get_smallest_node(table, visited_nodes):
+    """
+        Возвращает ближайший непосещенный элемент таблицы Дейкстры
+        Params:
+            table - таблица Дейкстры
+            visited_nodes - посещенные элементы
+        Return:
+            ближайший непосещенный узел
+    """
+    smallest = None
+    current_distance = None
+    for node, info in table.items():
+        if node in visited_nodes:
+            continue
+        distance = info["distance"]
+        if (not current_distance) or (distance < current_distance):
+            current_distance = distance
+            smallest = info["node"]
+    return smallest
+
+
+def _get_root_distance(root_node, table):
+    """
+        Возвращает дистанцию до текущего элемента из таблицы Дейкстры
+        Params:
+            root_node - текущий узел от которого заполняется таблица
+            table - таблица Дейкстры. 
+                    Если отсутствует - создается новая.
+        Return:
+            расстояние до текущего элемента
+    """
+    root_distance = 0
+    root_id = id(root_node)
+    if root_id in table:
+        root_distance = table[root_id]["distance"]
+    return root_distance
+
+
+def _calculate_table(root_node, table=None):
+    """
+        Заполнение таблицы Дейкстры
+        Params:
+            root_node - текущий узел от которого заполняется таблица
+            table - таблица Дейкстры. 
+                    Если отсутствует - создается новая.
+        Return:
+            таблица Дейкстры
+    """
+    if table is None:
+        table = {}
+    root_distance = _get_root_distance(root_node, table)
+    # Обход соседей текущего элемента
+    for neighbor in root_node["child"]:
+        node = neighbor["node"]
+        # Дистанция до соседа с учетом дистанции до текущего элемента
+        distance = root_distance + neighbor["distance"]
+        node_id = id(node)
+        if node_id in table:
+            table_distance = table[node_id]["distance"]
+            if distance >= table_distance:
+                continue
+        table[node_id] = {"node": node, "distance": distance, "from": root_node}    
+    return table
+
+
+def dijkstra_search(root_node):
+    """
+        Поиск кротчайшего пути от root_node до других узлов
+        Params:
+            node - начальный узел
+        Return:
+            таблица кротчайших путей от root_node
+    """
+    visited_nodes = set()
+    table = _calculate_table(root_node)
+    next_node = _get_smallest_node(table, visited_nodes)
+    while next_node:
+        table = _calculate_table(next_node, table)
+        visited_nodes.add(id(next_node))
+        next_node = _get_smallest_node(table, visited_nodes)
+    return table
+
+
 # Самотестирование
 if __name__ == "__main__":
     # Простое тестирование binary_search
@@ -75,3 +158,10 @@ if __name__ == "__main__":
     you = {"name": "You", "child": [bob, alice, clare]}
     assert paggie == graph_search("BFS", you, lambda node: node["name"] == "Paggie")
     assert anuged == graph_search("DFS", you, lambda node: node["name"] == "Anuged")
+    # Тестовый граф для алгоритма Дейкстры
+    fin = {"name": "fin", "child": []}
+    a_node = {"name": "A", "child": [{"distance": 1, "node": fin}]}
+    b_node = {"name": "B", "child": [{"distance": 5, "node": fin}, {"distance": 3, "node": a_node}]}
+    start = {"name": "start", "child": [{"distance": 6, "node": a_node}, {"distance": 2, "node": b_node}]}
+    dijkstra_table = dijkstra_search(start)
+    print(dijkstra_table)
